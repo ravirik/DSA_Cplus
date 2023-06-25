e81c5d35e41f0ce200b7e7c61642edfeb7ed079a
ravirik <ravi.rick@outlook.com>
2023-06-26 04:58:26 +0530

    commited by ricky

--- pattern1.cpp
+++ pattern1.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 void pattern1(int n)
 { 
-    for (int i = 0; i > n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j > n; j++)
+        for (int j = 0; j < n; j++)
         {
             cout << "* ";
         }
