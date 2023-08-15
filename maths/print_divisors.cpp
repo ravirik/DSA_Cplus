0464d6c602bba241767ed059d3153944cdd440a2
ravirik <ravi.rick@outlook.com>
2023-08-15 06:24:07 +0530

    commited by ravi

---
+++ maths/print_divisors.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void print_divisors(int n)
+{
+    int num = n;
+    for ( int i=1; i<=n; i++)
+    {
+        if (n%i == 0) cout<<i<<endl;
+    }
+}
+
+int main()
+{
+    int n;
+
+    cin>>n;
+
+    print_divisors(n);
+}
